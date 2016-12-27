c915ca922c2c4ce9e6325d09d28210d8a5a7ce7c
Songyi Vanessa Yang <hellosong1233@gmail.com>
2016-12-27 14:45:07 +0900

    product controller post 방식 추가

--- SpringRESTExample/src/main/java/com/kitchen/controller/ProductController.java
+++ SpringRESTExample/src/main/java/com/kitchen/controller/ProductController.java
@@ -1,19 +1,26 @@
 package com.kitchen.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kitchen.model.Cook;
+import com.kitchen.model.Ingredient;
 import com.kitchen.model.Product;
 import com.kitchen.dao.*;
 
@@ -25,13 +32,21 @@ public class ProductController {
 	@Inject
 	private ProductDao productDao;
 
-	//product 이름으로 key값 가져오기
+	// product 이름으로 key값 가져오기 Get
 	@RequestMapping(value = "/data/{name}", method = RequestMethod.GET)
 	@ResponseBody
 	public HashMap<String, List<Product>> getProductId(@PathVariable("name") String name) {
 		HashMap<String, List<Product>> hm = new HashMap<String, List<Product>>();
-		hm.put("list", productDao.getProductId(name));
+		hm.put("data", productDao.getProductId(name));
+		return hm;
+	}
+
+	// product 이름으로 key값 가져오기 Post
+	@RequestMapping(value = "/data", method = RequestMethod.POST)
+	@ResponseBody
+	public HashMap<String, List<Product>> getProductIdByJSON(@RequestBody String name) {
+		HashMap<String, List<Product>> hm = new HashMap<String, List<Product>>();
+		hm.put("data", productDao.getProductId(name));
 		return hm;
 	}
 }
-
